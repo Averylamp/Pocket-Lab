c1c3208822f321e0e971df54e5bc5a1481d5e916
Mark <mark@larah.me>
2015-09-06 06:02:08 -0400

    FUCK IT

---
+++ Ratios.swift
@@ -0,0 +1,19 @@
+//
+//  Ratios.swift
+//  Draculapp
+//
+//  Created by Mark Larah on 06/09/2015.
+//  Copyright (c) 2015 magicmark. All rights reserved.
+//
+
+import Foundation
+
+class Ratios {
+    
+    
+    var plasmaCount: Int?
+    var redBloodCellCount: Int?
+    
+}
+
+let sharedInstance = Ratios()
